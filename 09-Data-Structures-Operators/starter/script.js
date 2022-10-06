30006783f6d89eae7fe63e3220610f2e4d95e1e0
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-10-06 15:09:59 +0700

    the spread operator(...)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -37,9 +37,58 @@ const restaurant = {
     address,}) {
     console.log(`order received! ${this.starterMenu[starterIndex]} and
      ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+  orderPasta: function(ing1,ing2,ing3){
+    console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   }
 };
 
+/*
+const arr = [7,8,9];
+const badNewArr = [1,2,arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1,2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// join 2 array
+const menu = [...restaurant.starterMenu,...restaurant.mainMenu];
+console.log(menu);
+
+// iterables: arrays, strings, maps, sets, NOT obj
+const str = 'Jonas';
+const letters = [...str, '','S.'];
+console.log(letters);
+console.log(...str);
+//console.log(`${...str} schmedtmann`); // error
+
+const ingredients = [prompt('Let\'s make pasta! Ingredient 1?'),
+prompt('Let\'s make pasta! Ingredient 2?'),prompt('Let\'s make pasta! Ingredient 3?')];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0],ingredients[1],ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+// object
+const newRestaurant = {foundedIn: 1998, ...restaurant, founder:'Guiseppe'}
+console.log(newRestaurant);
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+*/
+
+/*
 restaurant.orderDelivery({
   time: '22.30',
   address: 'Via del sole, 21',
@@ -74,6 +123,8 @@ console.log(a,b);
 // console.log(fri);
 const {fri:{open: o,close: c},} = openingHours;
 console.log(o,c);
+
+*/
 
 /*
 const arr = [2,3,4];
