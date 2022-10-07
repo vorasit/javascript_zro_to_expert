52f0cb9f29c485b538b5b6ad9983491aafe07f49
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-10-07 13:30:24 +0700

    reset parameter

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -40,8 +40,47 @@ const restaurant = {
   },
   orderPasta: function(ing1,ing2,ing3){
     console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients){
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   }
 };
+
+/*
+// spread , because on right side of =
+const arr = [1, 2, ...[3,4]];
+
+// reset because on left side of =
+const [a,b, ...others] = [1,2,3,4,5];
+console.log(a,b,others);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza,risotto,otherFood);
+
+// obj
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);
+
+// 2 function
+const add = function(...numbers) {
+  //console.log(numbers);
+  let sum = 0;
+  for(let i =0; i<numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+}
+add(2,3);
+add(5,3,7,2);
+add(8,2,5,3,2,1,4);
+
+const x = [23,5,7];
+add(...x);
+
+restaurant.orderPizza('mushrooms','onion','olives','spinach');
+restaurant.orderPizza('mushrooms');
+
+*/
 
 /*
 const arr = [7,8,9];
