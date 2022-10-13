78b7b001fa0ec42e63a841da9aaffe2301340278
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2022-10-13 16:49:24 +0700

    Looping Objects: Object Keys, Values, and Entries
    Looping Objects: Object Keys, Values, and Entries

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,6 +50,36 @@ const restaurant = {
   }
 };
 
+/*
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+*/
+
+
+/*
 // WITH optional chaining
 console.log(restaurant.openingHours.mon?.open);
 console.log(restaurant.openingHours.thu?.open);
@@ -72,6 +102,7 @@ console.log(users[0]?.name ?? 'User array empty');
 
 if(users.length > 0) console.log(users[0].name); 
 else console.log('user array empty');
+*/
 
 /*
 const rest1 = {
